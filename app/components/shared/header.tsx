d97f4262080be62f74820685346813edcffb14da
Justine-Frigo <justine0912@gmail.com>
2024-11-27 17:16:10 +0100

    change bottom border color according to our main accent

--- app/components/shared/header.tsx
+++ app/components/shared/header.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <>
-            <header className="border-b border-slate-900/70 flex items-center justify-center sticky">
+            <header className="border-b border-orange-900/70 flex items-center justify-center sticky">
                 <section className="flex flex-row text-slate-800 px-4 py-2 justify-between w-2/3">
                     <h1 className="md:text-lg lg:text-xl xl:text-2xl">
                         <Link href={"/"}>
