bdebb7ed15c2a70cc312a40af8b97e79424d852c
Justine-Frigo <justine0912@gmail.com>
2024-11-27 17:15:21 +0100

    change cursor to pointer when hovering boxes

--- app/components/home/categories.tsx
+++ app/components/home/categories.tsx
@@ -6,7 +6,7 @@ import {faBuilding, faBusinessTime, faCar} from "@fortawesome/free-solid-svg-ico
 export default function Categories() {
     return (
         <>
-            <section className="grid grid-cols-3 gap-2">
+            <section className="grid grid-cols-3 gap-2 *:cursor-pointer">
                 <article className="bg-indigo-100 rounded-lg text-indigo-800 font-medium flex flex-row items-center gap-2 pl-2 py-2 hover:text-indigo-100 hover:bg-indigo-700">
                     <FontAwesomeIcon icon={faCar}/>
                     Automotive
