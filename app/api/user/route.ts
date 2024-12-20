6bc5a0d5b7cf41d16e4b8ab6f5bd3d1f1bb2757a
manu-cj <carenjotje@gmail.com>
2024-12-20 15:03:30 +0100

    save

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -69,6 +69,7 @@ export async function POST(req: Request) {
         city,
         country,
         profilePicture: "", // Placeholder pour une future logique de photo de profil
+        
       },
     });
 
