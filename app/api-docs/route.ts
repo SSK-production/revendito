8e181f9d45c564f91a827b859d8c59686d95792c
manu-cj <carenjotje@gmail.com>
2025-04-21 15:32:20 +0200

    deploy

--- app/api-docs/route.ts
+++ app/api-docs/route.ts
@@ -1,18 +1,11 @@
-// app/api-docs/route.ts
-import swaggerUi from "swagger-ui-express";
-import swaggerSpec from "@/lib/swagger";
-import { createServer,  } from "http";
-import express from "express";
+import { NextResponse } from 'next/server';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-const app = express();
+export async function GET() {
+  const filePath = join(process.cwd(), 'swagger.json');
+  const fileContents = readFileSync(filePath, 'utf-8');
+  const json = JSON.parse(fileContents);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-export const GET = async () => {
-  const server = createServer(app);
-  server.listen(); // Swagger se sert de ça en interne
-    
-  return new Response(JSON.stringify({ msg: "Swagger running" }), {
-    status: 200,
-  });
-};
+  return NextResponse.json(json);
+}
